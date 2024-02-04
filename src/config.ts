b9983b82c77b4ade7da4d5e8ca0a6dd202583601
Warn <imwarn@163.com>
2024-02-04 15:25:49 +0800

    fix: website config

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
-  title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  title: '爱情的纹理',
+  author: 'Warn',
+  description: '记录自诩热爱生活的工程师 Warn 的简单生活，不定期更新写手',
+  keywords: 'Warn,Blog,lovetime,博客,前端,爱情的纹理',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
-  homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
-  twitterId: 'HiTw93',
-  githubId: 'tw93',
-  repo: 'tw93/weekly'
+  homePage: 'https://www.715721.xyz',
+  blogPage: 'https://www.715721.xyz',
+  twitterId: 'fexWarn',
+  githubId: 'WangEN',
+  repo: 'WangEn/weekly'
 };
 
